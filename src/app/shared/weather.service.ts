2385b95c66db5d31aa621bddf4dcefb55986a41d
fz <34762675+faizalzafri@users.noreply.github.com>
2021-07-18 23:35:25 +0530

    Closes #5

--- src/app/shared/weather.service.ts
+++ src/app/shared/weather.service.ts
@@ -1,6 +1,10 @@
-import { HttpClient } from '@angular/common/http'
+import { HttpClient, HttpParams } from '@angular/common/http'
 import { Injectable } from '@angular/core'
+import { Observable } from 'rxjs'
+import { map } from 'rxjs/operators'
 import { environment } from 'src/environments/environment'
+
+import { ICurrentWeather } from './interfaces'
 
 export interface ICurrentWeatherData {
   weather: [
@@ -24,9 +28,30 @@ export interface ICurrentWeatherData {
 })
 export class WeatherService {
   constructor(private http: HttpClient) {}
-  getCurrentWeather(city: string, country: string) {
-    return this.http.get<ICurrentWeatherData>(
-      `${environment.apiUrl}/data/2.5/weather?q=${city},${country}&appid=${environment.apiKey}`
-    )
+
+  getCurrentWeather(city: string, country: string): Observable<ICurrentWeather> {
+    const uriParams = new HttpParams()
+      .set('q', `${city},${country}`)
+      .set('appid', environment.apiKey)
+    return this.http
+      .get<ICurrentWeatherData>(`${environment.apiUrl}/data/2.5/weather`, {
+        params: uriParams,
+      })
+      .pipe(map((data) => this.transformToICurrentWeather(data)))
+  }
+
+  private transformToICurrentWeather(data: ICurrentWeatherData): ICurrentWeather {
+    return {
+      city: data.name,
+      country: data.sys.country,
+      date: data.dt * 1000,
+      image: `${environment.imgUrl}/img/wn/${data.weather[0].icon}.png`,
+      temperature: this.convertKelvinToFahrenheit(data.main.temp),
+      description: data.weather[0].description,
+    }
+  }
+
+  private convertKelvinToFahrenheit(kelvin: number): number {
+    return (kelvin * 9) / 5 - 459.67
   }
 }
