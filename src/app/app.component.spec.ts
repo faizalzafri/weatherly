1bd84197889819b51134ee66c260a0bf1f7d2add
fz <34762675+faizalzafri@users.noreply.github.com>
2021-07-29 00:31:04 +0530

    ut using mocks

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,22 +1,14 @@
 import { TestBed } from '@angular/core/testing'
 import { RouterTestingModule } from '@angular/router/testing'
+import { createComponentMock } from 'angular-unit-test-helper'
 
 import { AppComponent } from './app.component'
-import { CurrentWeatherComponent } from './current-weather/current-weather.component'
-import { WeatherService } from './shared/weather.service'
-import { WeatherServiceFake } from './shared/weather.service.fake'
 
 describe('AppComponent', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       imports: [RouterTestingModule],
-      declarations: [AppComponent, CurrentWeatherComponent],
-      providers: [
-        {
-          provide: WeatherService,
-          useClass: WeatherServiceFake,
-        },
-      ],
+      declarations: [AppComponent, createComponentMock('CurrentWeatherComponent')],
     }).compileComponents()
   })
 
