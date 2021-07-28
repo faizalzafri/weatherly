d30d5a72a2cee288f2924e45021d5dc379d324e4
fz <34762675+faizalzafri@users.noreply.github.com>
2021-07-28 20:46:38 +0530

    configure use of fake

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,12 +1,22 @@
 import { TestBed } from '@angular/core/testing'
 import { RouterTestingModule } from '@angular/router/testing'
+
 import { AppComponent } from './app.component'
+import { CurrentWeatherComponent } from './current-weather/current-weather.component'
+import { WeatherService } from './shared/weather.service'
+import { WeatherServiceFake } from './shared/weather.service.fake'
 
 describe('AppComponent', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       imports: [RouterTestingModule],
-      declarations: [AppComponent],
+      declarations: [AppComponent, CurrentWeatherComponent],
+      providers: [
+        {
+          provide: WeatherService,
+          useClass: WeatherServiceFake,
+        },
+      ],
     }).compileComponents()
   })
 
@@ -16,18 +26,10 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy()
   })
 
-  it(`should have as title 'weatherly'`, () => {
-    const fixture = TestBed.createComponent(AppComponent)
-    const app = fixture.componentInstance
-    expect(app.title).toEqual('weatherly')
-  })
-
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent)
     fixture.detectChanges()
     const compiled = fixture.nativeElement as HTMLElement
-    expect(compiled.querySelector('.content span')?.textContent).toContain(
-      'weatherly app is running!'
-    )
+    expect(compiled.querySelector('div h1')?.textContent).toContain('Weatherly')
   })
 })
