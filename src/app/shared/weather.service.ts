fdc86b067d42c64f86db37a88e2302352fec9ca4
fz <34762675+faizalzafri@users.noreply.github.com>
2021-07-28 20:44:17 +0530

    Implemented a basic fake

--- src/app/shared/weather.service.ts
+++ src/app/shared/weather.service.ts
@@ -23,10 +23,14 @@ export interface ICurrentWeatherData {
   name: string
 }
 
+export interface IWeatherService {
+  getCurrentWeather(city: string, country: string): Observable<ICurrentWeather>
+}
+
 @Injectable({
   providedIn: 'root',
 })
-export class WeatherService {
+export class WeatherService implements IWeatherService {
   constructor(private http: HttpClient) {}
 
   getCurrentWeather(city: string, country: string): Observable<ICurrentWeather> {
